3b9a68b1d2da8ef938565ab214a790ab1b69b26d
macymuhia <macymuhia@gmail.com>
2019-08-13 09:26:49 +0300

    Refine the add blog form

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,32 +1,45 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
 from wtforms.validators import Required
-
 
 
 class BlogForm(FlaskForm):
 
-    blog_title = StringField('Place your blog title here',validators=[Required()])
-    blog_description = StringField('Give a brief blog description',validators=[Required()])
-    story = TextAreaField('Give the blog content',validators=[Required()])
-    category = SelectField('Category', choices=[('Gaming','Gaming'),('Career','Career'),('Technology','Technology'),('Sports','Sports'),('Fitness','Fitness')], validators=[Required()])
-    submit = SubmitField('Post')
+    blog_title = StringField("Blog title", validators=[Required()])
+    blog_description = StringField("Blog description", validators=[Required()])
+    story = TextAreaField("Blog content", validators=[Required()])
+    category = SelectField(
+        "Category",
+        choices=[
+            ("Gaming", "Gaming"),
+            ("Career", "Career"),
+            ("Technology", "Technology"),
+            ("Sports", "Sports"),
+            ("Fitness", "Fitness"),
+        ],
+        validators=[Required()],
+    )
+    submit = SubmitField("Post")
+
 
 class CommentForm(FlaskForm):
 
-    details = TextAreaField('Your comment',validators=[Required()])
+    details = TextAreaField("Your comment", validators=[Required()])
 
-    submit = SubmitField('Comment')
+    submit = SubmitField("Comment")
 
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
+    bio = TextAreaField("Tell us about you.", validators=[Required()])
+    submit = SubmitField("Submit")
+
 
 # class DeletePost(FlaskForm):
 #     comment_id = StringField()
 #     delete = SubmitField('Delete')
 
+
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
+    bio = TextAreaField("Tell us about you.", validators=[Required()])
+    submit = SubmitField("Submit")
+
